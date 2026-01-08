19524c979af976e871d8f73f3d164c2fd53c8aa5
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 14:46:20 -0600

    Fixed Commenting method

--- sandbox.js
+++ sandbox.js
@@ -7,12 +7,13 @@ let year = 2026;
 
 console.log(age, year);
 
-// age = 24;
-// console.log(age);
+/* age = 24;
+console.log(age);
 
-// const points = 100;
-// points = 50; Won't work on constants
-// console.log(points);
+const points = 100;
+points = 50; Won't work on constants
+console.log(points);
+*/
 
 var score = 75;
 console.log(score);
