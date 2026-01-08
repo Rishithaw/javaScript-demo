fbc63d380500448ee5a387695f7b29ee40a6fa70
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 14:43:14 -0600

    Used variables

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,18 @@
-console.log(1);
-console.log(2);
+// Variables can start with: letters, $, _
+// Variables cannot start with: numbers
+// Variables are case-sensitive
+let age = 19;
+let myAge = 19;
+let year = 2026;
+
+console.log(age, year);
+
+// age = 24;
+// console.log(age);
+
+// const points = 100;
+// points = 50; Won't work on constants
+// console.log(points);
+
+var score = 75;
+console.log(score);
