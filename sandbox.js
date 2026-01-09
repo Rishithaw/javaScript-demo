62d1b27a41156b3dd83a27a18a538194380b988f
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 20:16:36 -0600

    Used else if statements

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,11 @@
-// if statements
-// const age = 25;
-
-// if (age > 20) {
-//     console.log('You are over 20 years old.');
-// }
-
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-// if (ninjas.length > 3) {
-//     console.log("That's a lot of ninjas!");
-// }
+// else if statements
 
 const password = 'p@ss';
 
-if (password.length >= 8) {
-    console.log('That password is long enough.');
+if (password.length >= 12) {
+  console.log('That password is mighty strong.');
+} else if (password.length >= 8) {
+  console.log('That password is long enough.');
+} else {
+  console.log('password is not long enough.');
 }
