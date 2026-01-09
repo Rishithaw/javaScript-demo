ad2848aec521753ca4033e7ca2592137a585ddb3
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 20:06:08 -0600

    Used while loops

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,13 @@
-// For Loops
-// for (let i = 0; i < 5; i++) {
+// while Loops
+const names = ['shaun', 'mario', 'luigi'];
+// let i = 0;
+// while (i < 5) {
 //     console.log('In loop:', i);
+//     i++;
 // }
 
-// console.log('Loop finished');
-const names = ['shaun', 'mario', 'luigi'];
-for (let i = 0; i < names.length; i++) {
-    // console.log(names[i]);
-    let html = `<div>${names[i]}</div>`;
-    console.log(html);
+let i = 0;
+while (i < names.length) {
+    console.log(names[i]);
+    i++;
 }
