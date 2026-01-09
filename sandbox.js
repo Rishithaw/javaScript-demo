6bc0ed6998a21e96aaadc318b7b7891ffef42dbf
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 20:20:08 -0600

    Used logical operators

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,11 @@
-// else if statements
+// Logical Operators - OR || and AND &&
 
-const password = 'p@ss';
+const password = 'p@ss12';
 
-if (password.length >= 12) {
+if (password.length >= 12 && password.includes('@')) {
   console.log('That password is mighty strong.');
-} else if (password.length >= 8) {
-  console.log('That password is long enough.');
+} else if (password.length >= 8 || password.includes('@') && password.length >= 5) {
+  console.log('That password is strong enough.');
 } else {
-  console.log('password is not long enough.');
+  console.log('password is not strong enough.');
 }
