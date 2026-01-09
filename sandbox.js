1372139e0bf6ffcb1c0d5f97d09c2e979446b427
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 22:24:39 -0600

    Used an array of objects

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,18 @@
 // Object literals
 
+// const blogs = [
+//   {title:'why mac & cheese rules', likes: 30},
+//   {title: '10 things to make with marmite', likes: 50}
+// ];
+
 let user = {
   name: 'crystal',
   age: 30,
   email: 'crystal@thenetninja.co.uk',
   location: 'berlin',
-  blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+  blogs: [
+    {title:'why mac & cheese rules', likes: 30},
+    {title: '10 things to make with marmite', likes: 50}],
   login() {
     console.log('The user logged in');
   },
@@ -16,7 +23,7 @@ let user = {
     // console.log(this.blogs);
     console.log('This user has written the following blogs:');
     this.blogs.forEach(blog => {
-      console.log(blog);
+      console.log(blog.title, blog.likes);
     });
   }
 };
