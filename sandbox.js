6d536e86c97edce933c0b02e641ed4aa2fcb60cb
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-09 11:27:06 -0600

    Used the other document methods

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,15 @@
-// const para = document.querySelector('body > h1');
+// Get element by ID
+const title = document.getElementById('page-title');
+console.log(title);
 
-// console.log(para);
-
-const paras = document.querySelectorAll('p');
-const errors = document.querySelectorAll('.error');
-
+// Get elements by their class name
+const errors = document.getElementsByClassName('error');
 console.log(errors);
+console.log(errors[0]);
+
+
+// Get elements by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
+
