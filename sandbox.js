1860bc61280fa4a1e14be67cf8f170958ca5d488
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 20:08:49 -0600

    Used do while loop

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,9 @@
-// while Loops
+// do while Loops
 const names = ['shaun', 'mario', 'luigi'];
-// let i = 0;
-// while (i < 5) {
-//     console.log('In loop:', i);
-//     i++;
-// }
 
-let i = 0;
-while (i < names.length) {
-    console.log(names[i]);
-    i++;
-}
+let i = 3;
+
+do {
+  console.log('val of i is: ', i);
+  i++;
+} while (i < 5);
