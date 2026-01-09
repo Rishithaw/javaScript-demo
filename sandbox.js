b2e2eae58a090e8c1d4d60331ec7179c317ebcee
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 20:26:25 -0600

    Used break and continue keywords

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,17 @@
-// Logical NOT (!)
+// break and continue
 
-let user = false;
-if (!user) {
-  console.log("You must be logged in");
+const scores = [50, 25, 0, 30, 100, 20, 10];
+
+for (let i = 0; i < scores.length; i++) {
+
+  if (scores[i] === 0) {
+    continue;
+  }
+
+  console.log('Your score:', scores[i]);
+
+  if (scores[i] === 100) {
+    console.log('You got the top score!');
+    break;
+  }
 }
-
-console.log(!true);
-console.log(!false);
