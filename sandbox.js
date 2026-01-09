7554b6a7e4536e4d1cb19c8629353dc670242abe
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-09 13:28:18 -0600

    Added CSS without changing exsiting CSS

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,11 @@
-const link = document.querySelector('a');
+const title = document.querySelector('h1');
 
-console.log(link.getAttribute('href'));
-link.setAttribute('href', 'htps://www.thenetninjs.co.uk');
-link.innerText = 'The Net Ninja Website';
+// title.setAttribute('style', 'margin:50px');
 
-const msg = document.querySelector('p');
+console.log(title.style);
+console.log(title.style.color);
 
-console.log(msg.getAttribute('class'));
-msg.setAttribute('class', 'success');
-msg.setAttribute('style', 'color: green');
+title.style.margin = '50px';
+title.style.color = 'crimson';
+title.style.fontSize = '60px'
+title.style.margin = '';
