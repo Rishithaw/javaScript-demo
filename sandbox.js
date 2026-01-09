7e71daea5a561d30e2d9438d8a5f3986c746b5d2
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 19:40:39 -0600

    Used booleans

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,31 @@
-let age = null;
-console.log(age, age+3, `the age is ${age}`);
+// booleans & comparisons
+console.log(true, false, "true", "false");
+
+// Method that returns a boolean
+let email = "johndoe@example.com";
+let names = ["john", "jane", "doe"];
+
+// let result = email.includes("!");
+// let result = names.includes("james");
+
+
+// console.log(result);
+
+// Comparison operators
+let age = 20;
+
+// console.log(age == 20);
+// console.log(age == 30);
+// console.log(age != 30);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 20);
+// console.log(age >= 20);
+
+let name = "shaun";
+
+console.log(name == "shaun");
+console.log(name == "Shaun");
+console.log(name > "crystal");
+console.log(name > "Shaun");
+console.log(name > "Crystal");
