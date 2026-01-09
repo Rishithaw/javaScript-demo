ff9245e23d21bdde682ec9358d9ee5fc8e8b99dd
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 19:32:28 -0600

    Used Null and undefined

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,2 @@
-let names = ['Alice', 'Bob', 'Charlie'];
-
-// names[1] = 'David';
-// console.log(names[1]);
-
-// let ages = [20, 25, 30, 35];
-// console.log(ages[2]);
-
-// let random = ['shaun','crystal',30, true];
-// console.log(random);
-
-// Array methods
-// let result = names.join(' - ');
-// console.log(result);
-// let result = names.indexOf('Charlie');
-// let result = names.concat(['David', 'Eva']);
-let result = names.push('Eva');
-result = names.pop();
-console.log(result);
+let age = null;
+console.log(age, age+3, `the age is ${age}`);
