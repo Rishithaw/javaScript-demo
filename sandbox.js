891e389fa9352ab7077ce98c0ac349765dbe08c3
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 19:44:40 -0600

    Compared loose and strict equality comparision

--- sandbox.js
+++ sandbox.js
@@ -1,31 +1,15 @@
-// booleans & comparisons
-console.log(true, false, "true", "false");
+let age = 25;
 
-// Method that returns a boolean
-let email = "johndoe@example.com";
-let names = ["john", "jane", "doe"];
+// Loose equality comparison (Different types allowed)
 
-// let result = email.includes("!");
-// let result = names.includes("james");
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log(age != 25);
+// console.log(age != '25');
 
+// Strict equality comparison (Different types not allowed)
 
-// console.log(result);
-
-// Comparison operators
-let age = 20;
-
-// console.log(age == 20);
-// console.log(age == 30);
-// console.log(age != 30);
-// console.log(age > 20);
-// console.log(age < 20);
-// console.log(age <= 20);
-// console.log(age >= 20);
-
-let name = "shaun";
-
-console.log(name == "shaun");
-console.log(name == "Shaun");
-console.log(name > "crystal");
-console.log(name > "Shaun");
-console.log(name > "Crystal");
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25');
