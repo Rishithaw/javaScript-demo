42a736615f006bf18ff9795673561c6efd19116a
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 22:31:17 -0600

    Used the math object

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,21 @@
-// Object literals
+// Math Object
 
-// const blogs = [
-//   {title:'why mac & cheese rules', likes: 30},
-//   {title: '10 things to make with marmite', likes: 50}
-// ];
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
 
-let user = {
-  name: 'crystal',
-  age: 30,
-  email: 'crystal@thenetninja.co.uk',
-  location: 'berlin',
-  blogs: [
-    {title:'why mac & cheese rules', likes: 30},
-    {title: '10 things to make with marmite', likes: 50}],
-  login() {
-    console.log('The user logged in');
-  },
-  logout() {
-    console.log('The user logged out');
-  },
-  logBlogs() {
-    // console.log(this.blogs);
-    console.log('This user has written the following blogs:');
-    this.blogs.forEach(blog => {
-      console.log(blog.title, blog.likes);
-    });
-  }
-};
+const area = 7.7;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+// Random numbers
+
+const random = Math.random();
+
+console.log(random);
+console.log(Math.round (random* 100));
+
+
