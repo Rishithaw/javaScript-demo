a0d581934b82264364004f0cadc5019020acf137
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2026-01-08 22:13:53 -0600

    Added methods to an object literal

--- sandbox.js
+++ sandbox.js
@@ -5,17 +5,15 @@ let user = {
   age: 30,
   email: 'crystal@thenetninja.co.uk',
   location: 'berlin',
-  blogs: ['why mac & cheese rules', '10 things to make with marmite']
+  blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+  login: function() {
+    console.log('The user logged in');
+
+  },
+  logout: function() {
+    console.log('The user logged out');
+  }
 };
 
-console.log(user);
-console.log(user.name);
-
-//user.age = 35;
-console.log(user.age);
-
-console.log(user['location']);
-user['name'] = 'chum-li';
-console.log(user['name']);
-
-console.log(typeof user);
+user.login();
+user.logout();
